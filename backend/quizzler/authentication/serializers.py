06b5e276e8c5e698c52c7ebcec85e65810db5c5a
Alejandro Pacheco <alexpacheco4455@gmail.com>
2025-04-07 18:58:41 -0700

    Implemented register and login endpoints

---
+++ backend/quizzler/authentication/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from .models import CustomUser
+from django.contrib.auth import authenticate
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'username', 'password']
+        # password set to write only such that it can only be provided when creating a user but not in API responses
+        extra_kwargs = {'password': {'write_only': True}}
+
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password'],
+            username=validated_data['username']
+        )
+        
+        return user
+    
+    
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+    
+    def validate(self, credentials):
+        # retuns CustomUser object if account is present in DB, returns None if not present in DB
+        user = authenticate(email=credentials['email'], password=credentials['password'])
+        
+        # raise Error if account it not in DB
+        if user is None:
+            raise serializers.ValidationError("Invalid login credentials")
+        
+        # Return the existing validated user in a dict for further processing in LoginView
+        data = {
+        'user': user,
+        }
+        return data
