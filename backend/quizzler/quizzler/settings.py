332a33d95c1a9b0d208a3de93952e3fd219417c7
Alejandro Pacheco <alexpacheco4455@gmail.com>
2025-05-05 18:44:47 -0700

    Added logging middleware for render loggs

--- backend/quizzler/quizzler/settings.py
+++ backend/quizzler/quizzler/settings.py
@@ -78,6 +78,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'quizzler.middleware.LogRequestsMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
