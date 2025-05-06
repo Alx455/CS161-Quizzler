5156c345bea349ebaa410485fc6ddf44a09bd1e2
Alejandro Pacheco <alexpacheco4455@gmail.com>
2025-05-05 23:44:16 -0700

    websocket debugging

--- backend/quizzler/quizzler/asgi.py
+++ backend/quizzler/quizzler/asgi.py
@@ -1,10 +1,14 @@
 import os
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-from live_game_session.routing import websocket_urlpatterns  # adjust path if needed
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quizzler.settings')
+django.setup()
+
+from live_game_session.routing import websocket_urlpatterns
+
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
